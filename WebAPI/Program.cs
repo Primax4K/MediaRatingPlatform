c7aa972ea5344feb1ff868c051d4d2a6bbe2ddfe
Markus Kainz <m.kainz@htlkrems.at>
2025-10-18 19:32:45 +0200

    Refactor Program.cs to enhance dependency injection and configuration setup

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,36 +1,53 @@
-﻿using Microsoft.Extensions.Configuration;
-
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .Build();
+﻿using Domain.ConnectionFactory;
+using Domain.Repositories.Implementations;
+using Domain.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 var services = new ServiceCollection();
-services.AddSingleton<IConfiguration>(config);
+
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+services.AddSingleton<IConfiguration>(configuration);
+
+
+services.AddScoped<IAppUserRepository, AppUserRepository>();
+services.AddScoped<IGenreRepository, GenreRepository>();
+services.AddScoped<IMediaRepository, MediaRepository>();
+services.AddScoped<IRatingRepository, RatingRepository>();
+services.AddScoped<IRatingLikeRepository, RatingLikeRepository>();
+services.AddScoped<IFavoriteRepository, FavoriteRepository>();
+
+
+services.AddSingleton<IDbConnectionFactory>(_ =>
+    new NpgsqlConnectionFactory(
+        configuration.GetConnectionString("DefaultConnection")
+        ?? throw new Exception("No connection string found.")));
+
 
 services.AddSingleton<UserRouter>();
-
 services.AddSingleton<RouterHandler>();
 
-var provider = services.BuildServiceProvider();
 
+await using var provider = services.BuildServiceProvider();
 var routerHandler = provider.GetRequiredService<RouterHandler>();
-var configuration = provider.GetRequiredService<IConfiguration>();
 
-HttpListener listener = new HttpListener();
 
-string serverUrl = configuration["server"] ?? "http://localhost:8080/";
-
+var listener = new HttpListener();
+var serverUrl = configuration["server"] ?? "http://localhost:8080/";
 listener.Prefixes.Add(serverUrl);
 listener.Start();
 Console.WriteLine($"Listening on {serverUrl}");
 
 while (true)
 {
-    HttpListenerContext context = await listener.GetContextAsync();
+    var context = await listener.GetContextAsync();
     _ = Task.Run(async () =>
     {
         var request = context.Request;
         var response = context.Response;
+        
         try
         {
             await routerHandler.Dispatch(request, response);
